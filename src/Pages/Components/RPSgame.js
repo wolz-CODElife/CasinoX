241da00d7d3c569d2a56391a8d28a9af2d2ecd08
wolz-CODElife <joeladewole3@gmail.com>
2021-10-08 02:05:03 +0100

    Temp push

--- src/Pages/Components/RPSgame.js
+++ src/Pages/Components/RPSgame.js
@@ -1,10 +1,94 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './RPSgame.css'
 
-const RPSgame = () => {
+const RPSgame = ({players, setPlayers, gameRoundsLeft, setGameRoundsLeft}) => {
+    const [choices, setChoices] = useState()
+    const [ended, setEnded] = useState(false)
+
+    const Play = (yourChoice) => {
+        var player1Choice, player2Choice
+        player1Choice = yourChoice.id
+        player2Choice = numberToChoice(randToRpsInt())
+        let results = decideWinner(player1Choice, player2Choice)
+        let message = finalMessage(results)
+        rpsFrontend(player1Choice, player2Choice, message)
+    }
+    const randToRpsInt = () => {
+        return Math.floor(Math.random() * 3)
+    }
+    const numberToChoice = (number) => {
+        return ['rock', 'paper', 'scissors'][number]
+    }
+    const decideWinner = (yourChoice, opponentChoice) => {
+        var rpsDatabase = {
+            'rock': { 'scissors': 1, 'rock': 0.5, 'paper': 0 },
+            'paper': { 'rock': 1, 'paper': 0.5, 'scissors': 0 },
+            'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0}
+        }
+        var yourScore = rpsDatabase[yourChoice][opponentChoice]
+        var opponentScore = rpsDatabase[opponentChoice][yourChoice]
+        
+        return [yourScore, opponentScore]
+    }
+    const finalMessage = ([yourScore, opponentScore]) => {
+        if (yourScore === 0) {
+            setPlayers([{...players[0]}, {...players[1], score: players[1].score + 1}])
+            return {'message': 'You Lost', 'color':'red'}
+        } else if (yourScore === 0.5) {
+            return { 'message': 'You Tied', 'color': 'yellow' }
+        } else {
+            setPlayers([{...players[0], score: players[0].score + 1}, {...players[1]}])
+            return {'message': 'You Won', 'color': 'green'}
+        }
+    }
+    const rpsFrontend = (yourImageChoice, opponentImageChoice, finalMessage) => {
+        var imgDataBase = {
+            'rock': 'https://i.postimg.cc/Wp0GZFK4/image.png',
+            'paper': 'https://i.postimg.cc/15j35MHV/image.png',
+            'scissors': 'https://i.postimg.cc/T3Ngxryt/image.png'
+        }
+        setChoices({
+            player1Choice: imgDataBase[yourImageChoice],
+            player2Choice: imgDataBase[opponentImageChoice],
+            message: finalMessage
+        })
+        setGameRoundsLeft(gameRoundsLeft-1)
+        setEnded(true)
+        setTimeout(() => setEnded(false), 1000)
+    }
     return (
         <>
-            <h1>Rock Paper Scissors</h1>
+            {!ended ?
+                <div className="game_board">
+                    <div className="player_card">
+                        <img src="https://i.postimg.cc/Wp0GZFK4/image.png" alt="" id="rock" onClick={(e) => Play(e.target)} />
+                    </div>
+                    <div className="player_card">
+                        <img src="https://i.postimg.cc/15j35MHV/image.png" alt="" id="paper" onClick={(e) => Play(e.target)} />
+                    </div>
+                    <div className="player_card">
+                        <img src="https://i.postimg.cc/T3Ngxryt/image.png" alt="" id="scissors" onClick={(e) => Play(e.target)} />
+                    </div>
+                </div>
+                :
+                <div className="game_board">
+                    {choices?.player1Choice &&
+                        <div className="player_card">
+                            <img src={choices?.player1Choice} alt="" />
+                        </div>
+                    }
+                    {choices?.message &&
+                        <div className="player_card">
+                            <h1 style={{ color: choices?.message['color'] }}>{ choices?.message['message'] }</h1>
+                        </div>
+                    }
+                    {choices?.player2Choice &&
+                        <div className="player_card">
+                            <img src={choices?.player2Choice} alt="" />
+                        </div>
+                    }
+                </div>
+            }
         </>
     )
 }
--- src/Pages/GamePlay.js
+++ src/Pages/GamePlay.js
@@ -5,27 +5,30 @@ import RPSgame from './Components/RPSgame'
 import './css/GamePlay.css'
 
 const GamePlay = () => {
+    const initialGameRounds = 3
     const { name } = useParams()
     const [game, setGame] = useState(
         {
             winner: null,
-            name: null
+            name: null,
         }
     )
-    const players = [
+    const [players, setPlayers] = useState([
         {
             id: 1,
             name: 'me',
-            image: '',
+            image: 'https://i.postimg.cc/kG3v2r3C/image.png',
             score: 0
         },
         {
             id: 2,
-            name: 'computer',
-            image: '',
+            name: 'opponent',
+            image: 'https://i.postimg.cc/cHDFCk14/image.png',
             score: 0
         }
-    ]
+    ])
+    const [gameRoundsLeft, setGameRoundsLeft] = useState(initialGameRounds)
+    const [gameOver, setGameOver] = useState(false)
 
     useEffect(() => {
         switch (name) {
@@ -38,7 +41,19 @@ const GamePlay = () => {
             default:
                 break;
         }
-    }, [game])
+        if (gameRoundsLeft < 1) {
+            if (players[0].score > players[1].score) {
+                setGame({ ...game, winner: players[0].name })
+                
+            }else if (players[0].score === players[1].score) {
+                setGame({...game, winner: null})
+            }
+            else {
+                setGame({...game, winner: players[1].name})
+            }
+            setTimeout(() => setGameOver(true), 1000)
+        }
+    }, [name, players, gameRoundsLeft])
 
     return (
         <>
@@ -47,19 +62,32 @@ const GamePlay = () => {
                 <div className="playerslist">
                     <h2>Players</h2>
                     {players.map(player => (
-                        <div className="player_card">
+                        <div className="player_card" key={player.id}>
                             <img src={player.image} alt="" />
                             <h2>{player.name} <span>{ player.score }</span></h2>
                         </div>
                     ))}
                 </div>
-                <div className="game_playing">
-                    {
-                        name === 'rps' ? <RPSgame />:
-                            name === 'blackjack' ? '' :
-                                ''
-                    }
-                </div>
+                {!gameOver ?
+                    <div className="game_playing">
+                        {
+                            name === 'rps' ? <RPSgame players={players} setPlayers={setPlayers} gameRoundsLeft={gameRoundsLeft} setGameRoundsLeft={setGameRoundsLeft} />:
+                                name === 'blackjack' ? '' :
+                                    ''
+                        }
+                    </div>
+                :
+                    <div className="game_winner">
+                        {game.winner ?
+                            <h2>The winner is { game.winner === 'me' ? 'You': 'The Opponent' }</h2>
+                        :
+                            <h2>You tied</h2>
+                        }
+                        <div className="exit_button">
+                            <button>Exit</button>
+                        </div>
+                    </div>
+                }
             </div>
         </>
     )
